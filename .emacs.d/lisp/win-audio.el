538752916bb4445d0992274646275a9069adec2d
SlimTim10 <slimtim10@gmail.com>
2021-01-05 13:03:09 -0500

    Switch windows audio

---
+++ .emacs.d/lisp/win-audio.el
@@ -0,0 +1,12 @@
+;; Depends on nircmd
+;; https://www.nirsoft.net/utils/nircmd.html
+
+;; Use speakers
+(defun win-audio-speakers ()
+  (interactive)
+  (shell-command "nircmd setdefaultsounddevice \"Speakers\" 1"))
+
+;; Use headset
+(defun win-audio-headset ()
+  (interactive)
+  (shell-command "nircmd setdefaultsounddevice \"Headset Earphone\" 1"))
